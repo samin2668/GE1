f10258257bf8cdd8bc5d9c23c1988561e5b7b98a
samin2668 <samin2668@gmail.com>
2020-10-08 00:12:52 -0500

    rand num generator

---
+++ randNum.cpp
@@ -0,0 +1,27 @@
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) 
+{
+    string file = argv[1];
+    ofstream myFile;
+    myFile.open(file);
+    
+    srand((unsigned) time(0));
+    int randomNumber;
+    for(int i = 0; i < 10; i++)
+    {
+        for (int index = 0; index < 1000000; index++) 
+        {
+            randomNumber = (rand() % 10000000) + 1;
+            myFile << randomNumber << endl;
+        }
+        myFile << endl << endl << endl;
+    }
+    myFile.close();
+    return 0;
+}
